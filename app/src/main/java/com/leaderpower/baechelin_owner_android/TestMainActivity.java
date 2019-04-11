c72cc1cc515c742f575bdcf4633f99e167bf392d
JunKyus <kuk941025@gmail.com>
2019-04-12 00:45:45 +0900

    show a list of activities on start activity

--- app/src/main/java/com/leaderpower/baechelin_owner_android/TestMainActivity.java
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/TestMainActivity.java
@@ -1,13 +1,42 @@
 package com.leaderpower.baechelin_owner_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.leaderpower.baechelin_owner_android.util.Constant;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TestMainActivity extends AppCompatActivity {
+    @BindView(R.id.test_listview)
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_main);
+
+        ButterKnife.bind(this);
+        ArrayList<String> activities = new ArrayList<>();
+        for (Class c : Constant.activities) activities.add(c.getSimpleName());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, activities);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(TestMainActivity.this, Constant.activities[i]);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/leaderpower/baechelin_owner_android/util/Constant.java
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/util/Constant.java
@@ -1,4 +1,8 @@
 package com.leaderpower.baechelin_owner_android.util;
 
+import com.leaderpower.baechelin_owner_android.MainActivity;
+import com.leaderpower.baechelin_owner_android.activity.LoginActivity;
+
 public class Constant {
+    public static final Class[] activities = {LoginActivity.class, MainActivity.class};
 }
