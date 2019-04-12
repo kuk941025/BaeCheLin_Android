568ed4a6d04de4c3c05212c8c6b95f13c0e2910a
JunKyus <34362701+kuk941025@users.noreply.github.com>
2019-04-12 18:37:56 +0900

    login activity edited

--- app/src/main/java/com/leaderpower/baechelin_owner_android/util/Constant.java
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/util/Constant.java
@@ -1,7 +1,6 @@
 package com.leaderpower.baechelin_owner_android.util;
-
-import com.leaderpower.baechelin_owner_android.MainActivity;
 import com.leaderpower.baechelin_owner_android.activity.LoginActivity;
+import com.leaderpower.baechelin_owner_android.activity.MainActivity;
 
 public class Constant {
     public static final Class[] activities = {LoginActivity.class, MainActivity.class};
