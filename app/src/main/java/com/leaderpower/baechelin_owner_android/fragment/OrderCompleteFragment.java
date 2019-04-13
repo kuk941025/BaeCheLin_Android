344bfd57b9ef1c808fbf4bbe41db1e8d1ee06da6
JunKyus <kuk941025@gmail.com>
2019-04-13 23:05:16 +0900

    date picker example added

--- app/src/main/java/com/leaderpower/baechelin_owner_android/fragment/OrderCompleteFragment.java
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/fragment/OrderCompleteFragment.java
@@ -1,5 +1,6 @@
 package com.leaderpower.baechelin_owner_android.fragment;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,13 +10,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.leaderpower.baechelin_owner_android.R;
 import com.leaderpower.baechelin_owner_android.adapter.OrderListAdapter;
 import com.leaderpower.baechelin_owner_android.model.Order;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +37,7 @@ public class OrderCompleteFragment extends Fragment {
     private OrderListAdapter orderAdapter;
     private View fragView = null;
     private ArrayList<Order> orderList;
+    private final Calendar calendar = Calendar.getInstance();
     public OrderCompleteFragment() {
         // Required empty public constructor
     }
@@ -45,11 +51,20 @@ public class OrderCompleteFragment extends Fragment {
             ButterKnife.bind(this, fragView);
 
             initRecycleView();
+            setDatePicker();
         }
 
         return fragView;
     }
+    private void setDatePicker(){
 
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getContext(), dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
     private void initRecycleView(){
         orderList = new ArrayList<>();
         for (int i = 0; i < 100; i++) orderList.add(new Order("주소 " + i, "음식 " + i));
@@ -59,4 +74,19 @@ public class OrderCompleteFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(orderAdapter);
     }
+
+    private void updateLabel() {
+        String format = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+
+        startDate.setText(sdf.format(calendar.getTime()));
+    }
+    DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            calendar.set(Calendar.YEAR, i);
+            calendar.set(Calendar.MONTH, i1);
+            calendar.set(Calendar.DAY_OF_MONTH, i2);
+        }
+    };
 }
