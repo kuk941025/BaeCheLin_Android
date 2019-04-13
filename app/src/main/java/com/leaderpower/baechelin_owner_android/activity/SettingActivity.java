a0d61b5fb084b78a58aaac048a7043a6fd205d9d
JunKyus <kuk941025@gmail.com>
2019-04-13 15:46:40 +0900

    setting activity layout ui added

---
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/activity/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.leaderpower.baechelin_owner_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.leaderpower.baechelin_owner_android.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
--- app/src/main/java/com/leaderpower/baechelin_owner_android/util/Constant.java
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/util/Constant.java
@@ -1,7 +1,8 @@
 package com.leaderpower.baechelin_owner_android.util;
 import com.leaderpower.baechelin_owner_android.activity.LoginActivity;
 import com.leaderpower.baechelin_owner_android.activity.MainActivity;
+import com.leaderpower.baechelin_owner_android.activity.SettingActivity;
 
 public class Constant {
-    public static final Class[] activities = {LoginActivity.class, MainActivity.class};
+    public static final Class[] activities = {LoginActivity.class, MainActivity.class, SettingActivity.class};
 }
