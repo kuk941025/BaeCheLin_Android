2a49059d5bf2aec03418dcdd664224ee9f6d8f9c
JunKyus <34362701+kuk941025@users.noreply.github.com>
2019-07-22 17:26:57 +0900

    order detail xml middle commit

---
+++ app/src/main/java/com/leaderpower/baechelin_owner_android/activity/OrderDetail.java
@@ -0,0 +1,15 @@
+package com.leaderpower.baechelin_owner_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.leaderpower.baechelin_owner_android.R;
+
+public class OrderDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail);
+    }
+}
